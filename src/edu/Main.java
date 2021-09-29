b895da866fe1eacfb9e013606b61fcfd4ea805c8
user <user@DESKTOP-35V2L75>
2021-09-29 22:43:19 +0900

    init Project

---
+++ src/edu/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package edu;
+
+/**
+ * @author user
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//변수 선언하기
+		int firstNo, secondNo;
+		int sum;
+		//두수 입력하기
+		firstNo =10;
+		secondNo = 20;
+		//합계내기(c=a+b)
+		sum = firstNo + secondNo;
+		//합계 출력하기
+		System.out.println("합계: " + sum);
+	}
+
+}
